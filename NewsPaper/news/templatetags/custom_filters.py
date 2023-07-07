dfc27160d9d163c84413a99447274e840bf4dec6
Artyom Rezer <118107094+artyomrezer@users.noreply.github.com>
2023-07-07 13:19:12 +0500

    Update custom_filters.py

--- NewsPaper/news/templatetags/custom_filters.py
+++ NewsPaper/news/templatetags/custom_filters.py
@@ -5,7 +5,7 @@ register = template.Library()
 @register.filter(name='censor')
 def censor(string):
     if isinstance(string, str):
-        bad_words_list = ['bad_word1', 'bad_word2', 'bad_word3', ...]
+        bad_words_list = ['bad_word1', 'bad_word2', 'bad_word3']
         #либо загрузить список плохих слов из внешнего файла.
 
         for bad_word in bad_words_list:
